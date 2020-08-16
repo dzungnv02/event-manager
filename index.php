d34c5875665d1d4922f13b15b774a9d77afe7766
dzungnv02 <dungnv02@gmail.com>
2020-08-16 08:58:53 +0700

    temprary commit

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once 'app/bootstrap.php';
+use App\User;
+
+$user = new User($dbConnection);
+$user->insert(['email'=> 'dzungnv02', 'username'=> 'Dzungnv02']);
+var_dump($user->findAll());
