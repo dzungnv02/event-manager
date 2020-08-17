50524190cf0c10c9829a9de266be9e94eb78f486
dzungnv02 <dungnv02@gmail.com>
2020-08-17 11:57:38 +0700

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-#error_reporting(E_ALL);
-#error_reporting(0);
-$conf = array();
-$conf['dbhost'] = getenv("DATABASE_HOST");
-$conf['dbusername'] = getenv("DATABASE_USER");
-$conf['dbpassword'] = getenv("DATABASE_PASSWORD");;
-$conf['dbname'] = getenv("DATABASE_NAME");;
-?>
